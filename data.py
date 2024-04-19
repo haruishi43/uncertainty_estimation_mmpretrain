249fdd86826dfb870d6ca3c5b34fe53c4f9bc3cf
haruishi43 <toraudonn@gmail.com>
2024-04-19 02:17:09 +0000

    still trying to reproduce the paper values; the mnist accuracy is a few points lower

---
+++ data.py
@@ -0,0 +1,46 @@
+import torch
+from torchvision.datasets.mnist import MNIST
+import torchvision.transforms as transforms
+from torch.utils.data import DataLoader
+
+data_train = MNIST(
+    "./data/mnist",
+    download=True,
+    train=True,
+    transform=transforms.Compose([transforms.ToTensor()]),
+)
+
+data_val = MNIST(
+    "./data/mnist",
+    train=False,
+    download=True,
+    transform=transforms.Compose([transforms.ToTensor()]),
+)
+
+
+def create_dataloader(
+    batch_size: int = 1024,
+    num_workers: int = 8,
+):
+    dataloader_train = DataLoader(
+        data_train,
+        batch_size=batch_size,
+        shuffle=True,
+        num_workers=num_workers,
+        drop_last=True,
+    )
+    dataloader_val = DataLoader(
+        data_val,
+        batch_size=batch_size,
+        num_workers=num_workers,
+        drop_last=False,
+    )
+
+    dataloaders = {
+        "train": dataloader_train,
+        "val": dataloader_val,
+    }
+    return dataloaders
+
+
+digit_one, _ = data_val[5]
