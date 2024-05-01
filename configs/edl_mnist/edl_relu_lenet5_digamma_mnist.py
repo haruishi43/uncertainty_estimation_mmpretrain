e010e0351dd4a4dfefc3720f071219ef3d223536
haruishi43 <toraudonn@gmail.com>
2024-05-01 11:57:40 +0000

    update

---
+++ configs/edl_mnist/edl_relu_lenet5_digamma_mnist.py
@@ -0,0 +1,28 @@
+_base_ = [
+    "../_base_/datasets/mnist_bs128.py",
+    "../_base_/schedules/mnist_sgd_bs128.py",
+    "../_base_/default_runtime.py",
+]
+
+# baseline (default) configuration
+
+model = dict(
+    type="EvidentialImageClassifier",
+    backbone=dict(
+        type="ModernLeNet5",
+        act_cfg=dict(type="ReLU"),
+    ),
+    neck=None,
+    head=dict(
+        type="EvidentialStackedLinearClsHead",
+        num_classes=10,
+        in_channels=120,
+        mid_channels=[84],
+        evidence_func="relu",
+        loss=dict(type="DirichletDigammaLoss"),
+    ),
+)
+
+custom_hooks = [
+    dict(type="PassStepInfoHook"),
+]
